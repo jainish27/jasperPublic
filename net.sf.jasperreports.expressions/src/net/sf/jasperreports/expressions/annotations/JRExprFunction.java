cc7787063f3d641c76950398830fa1150bd9dda7
teodord <teodord@5deaa220-7875-4d0c-92b4-1a5a5e3258bb>
2013-04-30 10:30:05 +0000

    JR update

--- net.sf.jasperreports.expressions/src/net/sf/jasperreports/expressions/annotations/JRExprFunction.java
+++ net.sf.jasperreports.expressions/src/net/sf/jasperreports/expressions/annotations/JRExprFunction.java
@@ -5,11 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import net.sf.jasperreports.functions.annotations.Function;
+
 /**
  * Annotation used to mark a method as function usable inside an expression editor. 
  * 
  * @author Massimo Rabbi (mrabbi@users.sourceforge.net)
- *
+ * @deprecated Replaced by {@link Function}.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
